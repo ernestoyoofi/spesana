95d7e699c6f2d1234cdc77ebb0e50f84e3fe4817
Ernestoyoofi <yoofi.ernesto@gmail.com>
2021-12-18 21:06:11 +0700

    database

---
+++ javascript/database/absen-murid.js
@@ -0,0 +1,39 @@
+/* ====== [ Jumlah Siswa Siswi Dan Data ] ====== */
+
+document.getElementById("java-load").innerHTML = "<b style='font-style: normal;font-weight: normal; background-color: #00b04b; padding: 3px; border-radius: 3px;'>Database Telah Terbaca <i class='fa fa-check load'></i></b>";
+document.getElementById("jumlah-murid").innerHTML = "32";
+document.getElementById("jumlah-siswa").innerHTML = "12";
+document.getElementById("jumlah-siswi").innerHTML = "20";
+/* ====== [ User ID ] ====== */
+document.getElementById("user-1").innerHTML = "E2-01hi01ef78";
+document.getElementById("user-2").innerHTML = "E2-02hj01ef78";
+document.getElementById("user-3").innerHTML = "E2-03hk01ef78";
+document.getElementById("user-4").innerHTML = "E2-04hl01ef78";
+document.getElementById("user-5").innerHTML = "";
+document.getElementById("user-6").innerHTML = "";
+document.getElementById("user-7").innerHTML = "";
+document.getElementById("user-8").innerHTML = "";
+document.getElementById("user-9").innerHTML = "";
+document.getElementById("user-10").innerHTML = "";
+document.getElementById("user-11").innerHTML = "";
+document.getElementById("user-12").innerHTML = "";
+document.getElementById("user-13").innerHTML = "";
+document.getElementById("user-14").innerHTML = "";
+document.getElementById("user-15").innerHTML = "";
+document.getElementById("user-16").innerHTML = "";
+document.getElementById("user-17").innerHTML = "";
+document.getElementById("user-18").innerHTML = "";
+document.getElementById("user-19").innerHTML = "";
+document.getElementById("user-20").innerHTML = "";
+document.getElementById("user-21").innerHTML = "";
+document.getElementById("user-22").innerHTML = "";
+document.getElementById("user-23").innerHTML = "";
+document.getElementById("user-24").innerHTML = "";
+document.getElementById("user-25").innerHTML = "";
+document.getElementById("user-26").innerHTML = "";
+document.getElementById("user-27").innerHTML = "";
+document.getElementById("user-28").innerHTML = "";
+document.getElementById("user-29").innerHTML = "";
+document.getElementById("user-30").innerHTML = "";
+document.getElementById("user-31").innerHTML = "";
+document.getElementById("user-32").innerHTML = "";
