8e975765b3d6c0c65e908808bff45ad8e226c1a8
Ernestoyoofi <yoofi.ernesto@gmail.com>
2021-12-19 11:43:48 +0700

    Add files via upload

--- javascript/consol-log.js
+++ javascript/consol-log.js
@@ -1,15 +1,8 @@
-console.log("Loading...")
-console.log("https://ernestoyoofi.github.io/kelas7bnanggulan To ➡ https://ernestoyoofi.github.io/spesana7b")
-console.log("[ !Question ] Website SMPN 1 Nanggulan ? ]")
-console.log("[ !Answers ] https://www.smpn1nanggulan.sch.id/")
-console.log("This Website Created By Ernesto.yoofi  https://github.com/ernestoyoofi")
-console.log("Version 1.5.4 Beta!")
-console.log("Tamplate Version : 1.5.4")
-console.log("Check Crashed File / Corrupt File In Here [ Consol Log ]")
-console.log("Powered High Loading Speed Open GL ES 2.0 By https://ernestoyoofi.github.io/load.js Version 8 JavaVM Mobile")
-console.log("Powered Server With https://pages.github.com & file://D:/Server/Local/Storages/SMPN-1-Nanggulan")
-console.log("======================================")
+console.error("English Language : Read console log on web app"); console.error("Bahasa Indonesia : Baca Console Log Pada Aplikasi Web"); console.warn("English Language : Database " + window.location.href + " can be access, how many people can access the database link, maybe this will make the data insecure, don't spread our privacy data, take care and stick to the rules and don't do hoaxes or news that makes privacy data exposed for no reason"); console.warn("Bahasa Indonesia : Database " + window.location.href +" dapat diakses, berapa banyak orang yang dapat mengakses link database, mungkin ini akan membuat data tidak aman, jangan menyebarkan data privasi kami, berhati-hatilah dan patuhi aturan dan jangan melakukan hoax atau berita yang membuat data privasi terekspos tanpa alasan");
+document.getElementById("Title-home").innerHTML="Kelas 7B | Home";
+document.getElementById("title-muridkelas").innerHTML="Kelas 7B | Siswa Siswi"
+console.log("======================================");
 console.log("Created By : Ernestoyoofi")
-console.log("Starting Upload First : 21:54:09 Waktu Indoneisa Bagian Barat [Yogyakarta] / 14 November 2021")
-console.log("Script : [HTML 89%][JS 1%][CSS 10]")
-console.log("======================================")
+console.log("Starting Upload First : 21:54:09 Waktu Indoneisa Bagian Barat [Yogyakarta] / 14 November 2021");
+console.warn("Version : 1.5.7");
+console.log("======================================");
