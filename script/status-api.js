30c974b39084e8a71827d6936b4a2151ced53d5d
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-01-07 14:26:40 +0700

    Script
    JS

--- script/status-api.js
+++ script/status-api.js
@@ -1,5 +1,5 @@
-var StatusAPI = "Aman, Tidak Ada Kesalahan"; var Informasi = "Terakhir Update Rabu 5 Jan 2020 / 16:08";
-document.getElementById("status-api").innerHTML = "<p style='color: #7aea3b'>" + StatusAPI + "</p>"; document.getElementById("status-protokol").innerHTML = "<p style='color: #7aea3b'>Telah Ditetapkan</p>"; document.getElementById("status-murid").innerHTML = "<p style='color: #7aea3b'>32, Siswa Dan Siswi Masuk Semua</p>";	 document.getElementById("terakhir-diupdate-api").innerHTML = "Terakhir Update : " + Informasi + "";
+var StatusAPI = "Troubleshoot, Maybe some scripts are being fixed"; var Informasi = "Terakhir Update Jumat 7 Januari 2022 14:21";
+document.getElementById("status-api").innerHTML = "<p style='color: #ff000a'>" + StatusAPI + "</p>"; document.getElementById("status-protokol").innerHTML = "<p style='color: #7aea3b'>Telah Ditetapkan</p>"; document.getElementById("status-murid").innerHTML = "<p style='color: #7aea3b'>32, Siswa Dan Siswi Masuk Semua</p>";	 document.getElementById("terakhir-diupdate-api").innerHTML = "Terakhir Update : " + Informasi + "";
 function GetStatusAPISpesana() {
   if (confirm("Apakah Kamu Ingin Cek Status API Kami Diconsole? ")) {
     console.log("Status API : \n\n- Status API : " + StatusAPI + "\n- Terakhir Update : " + Informasi + "")
