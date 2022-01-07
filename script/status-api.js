d622bcfb34a054ce9dcc696ac27335fcb0acb50d
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-01-07 14:40:42 +0700

    Script

--- script/status-api.js
+++ script/status-api.js
@@ -1,4 +1,4 @@
-var StatusAPI = "Troubleshoot, Maybe some scripts are being fixed"; var Informasi = "Terakhir Update Jumat 7 Januari 2022 14:21";
+var StatusAPI = "Error"; var Informasi = "Terakhir Update Jumat 7 Januari 2022 14:21";
 document.getElementById("status-api").innerHTML = "<p style='color: #ff000a'>" + StatusAPI + "</p>"; document.getElementById("status-protokol").innerHTML = "<p style='color: #7aea3b'>Telah Ditetapkan</p>"; document.getElementById("status-murid").innerHTML = "<p style='color: #7aea3b'>32, Siswa Dan Siswi Masuk Semua</p>";	 document.getElementById("terakhir-diupdate-api").innerHTML = "Terakhir Update : " + Informasi + "";
 function GetStatusAPISpesana() {
   if (confirm("Apakah Kamu Ingin Cek Status API Kami Diconsole? ")) {
