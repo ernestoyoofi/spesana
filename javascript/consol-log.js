8ac6393bb09a428e3bf45446fe0dc28cb9745500
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-01-07 12:35:53 +0700

    Delete javascript directory

--- javascript/consol-log.js
+++
@@ -1,8 +0,0 @@
-console.error("English Language : Read console log on web app"); console.error("Bahasa Indonesia : Baca Console Log Pada Aplikasi Web"); console.warn("English Language : Database " + window.location.href + " can be access, how many people can access the database link, maybe this will make the data insecure, don't spread our privacy data, take care and stick to the rules and don't do hoaxes or news that makes privacy data exposed for no reason"); console.warn("Bahasa Indonesia : Database " + window.location.href +" dapat diakses, berapa banyak orang yang dapat mengakses link database, mungkin ini akan membuat data tidak aman, jangan menyebarkan data privasi kami, berhati-hatilah dan patuhi aturan dan jangan melakukan hoax atau berita yang membuat data privasi terekspos tanpa alasan");
-document.getElementById("Title-home").innerHTML="Kelas 7B | Home";
-document.getElementById("title-muridkelas").innerHTML="Kelas 7B | Siswa Siswi"
-console.log("======================================");
-console.log("Created By : Ernestoyoofi")
-console.log("Starting Upload First : 21:54:09 Waktu Indoneisa Bagian Barat [Yogyakarta] / 14 November 2021");
-console.warn("Version : 1.5.7");
-console.log("======================================");
