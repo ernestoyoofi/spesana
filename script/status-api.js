f717d4e5bcc7b4feb6796a1aa499c79f53a5dc50
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-01-08 14:45:13 +0700

    JS

--- script/status-api.js
+++ script/status-api.js
@@ -1,5 +1,5 @@
-var StatusAPI = "Error"; var Informasi = "Terakhir Update Jumat 7 Januari 2022 14:21";
-document.getElementById("status-api").innerHTML = "<p style='color: #ff000a'>" + StatusAPI + "</p>"; document.getElementById("status-protokol").innerHTML = "<p style='color: #7aea3b'>Telah Ditetapkan</p>"; document.getElementById("status-murid").innerHTML = "<p style='color: #7aea3b'>32, Siswa Dan Siswi Masuk Semua</p>";	 document.getElementById("terakhir-diupdate-api").innerHTML = "Terakhir Update : " + Informasi + "";
+var StatusAPI = "Maintenance"; var Informasi = "Terakhir Update Jumat 8 Januari 2022 14:36";
+document.getElementById("status-api").innerHTML = "<p style='color: #ff9d00'>" + StatusAPI + "</p>"; document.getElementById("status-protokol").innerHTML = "<p style='color: #7aea3b'>Telah Ditetapkan</p>"; document.getElementById("status-murid").innerHTML = "<p style='color: #ff9d00'>Dibagi Menjadi 2 Sesi ( Pagi Dan Siang ) 50%</p>";	 document.getElementById("terakhir-diupdate-api").innerHTML = "Terakhir Update : " + Informasi + "";
 function GetStatusAPISpesana() {
   if (confirm("Apakah Kamu Ingin Cek Status API Kami Diconsole? ")) {
     console.log("Status API : \n\n- Status API : " + StatusAPI + "\n- Terakhir Update : " + Informasi + "")
@@ -8,3 +8,4 @@ function GetStatusAPISpesana() {
   }
   
 }
+/* ERR :#ff000a , Warn :#ff9d00 , Normal :#7aea3b*/
