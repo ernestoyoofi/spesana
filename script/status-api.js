662382b39c3597bd47e32d33bacbd0e847bc62d6
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-01-10 08:48:01 +0700

    JS

--- script/status-api.js
+++ script/status-api.js
@@ -1,5 +1,5 @@
-var StatusAPI = "Maintenance"; var Informasi = "Terakhir Update Jumat 8 Januari 2022 14:36";
-document.getElementById("status-api").innerHTML = "<p style='color: #ff9d00'>" + StatusAPI + "</p>"; document.getElementById("status-protokol").innerHTML = "<p style='color: #7aea3b'>Telah Ditetapkan</p>"; document.getElementById("status-murid").innerHTML = "<p style='color: #ff9d00'>Dibagi Menjadi 2 Sesi ( Pagi Dan Siang ) 50%</p>";	 document.getElementById("terakhir-diupdate-api").innerHTML = "Terakhir Update : " + Informasi + "";
+var StatusAPI = "Maintenance"; var Informasi = "Terakhir Update Senin 10 Januari 2022 05:02";
+document.getElementById("status-api").innerHTML = "<p style='color: #ff9d00'>" + StatusAPI + "</p>"; document.getElementById("status-protokol").innerHTML = "<p style='color: #7aea3b'>Telah Ditetapkan</p>"; document.getElementById("status-murid").innerHTML = "<p style='color: #ff9d00'>Dibagi Menjadi 2 Shift ( Pagi Dan Siang ) 50%</p>";	 document.getElementById("terakhir-diupdate-api").innerHTML = "Terakhir Update : " + Informasi + "";
 function GetStatusAPISpesana() {
   if (confirm("Apakah Kamu Ingin Cek Status API Kami Diconsole? ")) {
     console.log("Status API : \n\n- Status API : " + StatusAPI + "\n- Terakhir Update : " + Informasi + "")
