45725c4950b9c518cca59f4800d04ba660c84dee
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-02-13 11:28:12 +0700

    Add files via upload

---
+++ s/app.js
@@ -0,0 +1,10 @@
+function Start() {
+  console.log("Starting")
+  var Name = document.getElementById("file").value;
+  var Type = document.getElementById("type").value;
+  
+  var File = Name +"."+ Type ;
+  console.log(File)
+  
+  window.location.href = "https://storage-spesana.vercel.app/s/"+Type+"/"+File;
+}
