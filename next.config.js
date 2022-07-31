362755031d54112233df4baa4a5fb7c9330d87d4
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-07-31 10:18:06 +0700

    Test

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-// module.exports = nextConfig
-// const withPWA = require('next-pwa')
-// const runtimeCaching = require('next-pwa/cache')
-
-// module.exports = withPWA({
-//   pwa: {
-//     dest: 'public',
-//     runtimeCaching,
-//   },
-// })
-
-module.exports = {
-  //...
-  performance: {
-    hints: true,
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
   },
-};
+})
