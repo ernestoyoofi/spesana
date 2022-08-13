575f5c0c3688df5946cbaae36b13db09cb73a0fb
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-08-13 13:45:27 +0700

    Adding input form

--- pages/search.js
+++ pages/search.js
@@ -1,5 +1,8 @@
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { useEffect } from "react"
 import styles from '../styles/Home.module.css'
 // Component
 import Navbar from '../comp/navbar'
@@ -8,6 +11,9 @@ import ScrollTop from '../comp/srolltop'
 export default function Home() {
   const router = useRouter()
   const p = router.query.search_name
+  useEffect(() => {
+   document.querySelector("#autoinput").value = router.query.search_name || ""
+  })
   return (
     <div className={styles.container}>
       <Head>
@@ -15,7 +21,17 @@ export default function Home() {
         <meta name="description" content="Website Siswa SMPN 1 Nanggulan 7B" />
       </Head>
       <Navbar />
-      { p }
+      <main>
+        <div className={styles.respon_pr}>
+          <form action="/search" method="get" style={{ width: "100%", display: "flex" }}>
+            <input type="search" id="autoinput"  name="search_name" style={{ marginRight: "20px", width: "100%" }} placeholder="Ketik disini untuk mencari..." />
+            <button><FontAwesomeIcon icon={faSearch} className={styles.icon}/></button>
+          </form>
+          <ul>
+            {p? "": <li><center>Apa Yang Ingin Kamu Cari?</center></li>}
+          </ul>
+        </div>
+      </main>
       <div className={styles.loading} style={{ height: "100vh" }}>
         <div className={styles.loading_con}>
           <svg className={styles.loading_cyc}><circle cx="25" cy="25" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" className={styles.loading_par}/></svg>
